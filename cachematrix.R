63fd792f8d3154797d33c577ff32ac84c9fafdb2
nico vilni <nicolas.vilnitzky@gmail.com>
2024-01-09 11:57:41 -0300

    https://github.com/nicovilni/ProgrammingAssignment2

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,37 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+##
+##  "x" is going to be a matrix
+## "s" = null
+## then changed "mean" to "solve"
+makeCacheMatrix <- function(x = matrix(sample(1:100,9),3,3)) {
+  s <- NULL
+  set <- function(y) {
+    x <<- y
+    s <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) s <<- solve
+  getsolve <- function() s
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
+}
 }
 
 
 ## Write a short comment describing this function
-
+## I did the same as I did before, I change x to s and mean to solve
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        s <- x$getsolve()
+  if(!is.null(s)) {
+    message("getting inversed matrix")
+    return(s)
+  }
+  data <- x$get()
+  s <- solve(data, ...)
+  x$setsolve(s)
+  s
 }
